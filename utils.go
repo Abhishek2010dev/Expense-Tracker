7bd2daa60bb5922db5f21006663dd86049d43781
Abhishek <abhisekkumar2010dev@gmail.com>
2025-03-03 18:14:40 +0530

    GetFileTOWrite function

--- utils.go
+++ utils.go
@@ -2,24 +2,33 @@ package main
 
 import (
 	"encoding/csv"
+	"log"
 	"os"
 	"strconv"
 )
 
-func GetNextID(file *os.File) uint {
+func GetNextID(file *os.File) int {
 	reader := csv.NewReader(file)
 	records, err := reader.ReadAll()
 	if err != nil || len(records) < 1 {
 		return 1
 	}
 
-	var maxID uint = 0
+	var maxID int = 0
 	for i := 1; i < len(records); i++ {
-		id, err := strconv.ParseUint(records[i][0], 10, 32)
-		if err == nil && uint(id) > maxID {
-			maxID = uint(id)
+		id, err := strconv.Atoi(records[i][0])
+		if err == nil && id > maxID {
+			maxID = (id)
 		}
 	}
 
 	return maxID
+}
+
+func GetFileToWrite(filename string) *os.File {
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0664)
+	if err != nil {
+		log.Fatal("Cannot open or create csv file: %s, error: %v", filename, err)
+	}
+	return file
 }
