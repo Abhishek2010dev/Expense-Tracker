0230447dd6f7d9f83a3c8c75f19bed7c78341efc
Abhishek <abhisekkumar2010dev@gmail.com>
2025-03-03 18:18:02 +0530

    using folder

---
+++ utils/file.go
@@ -0,0 +1,14 @@
+package utils
+
+import (
+	"log"
+	"os"
+)
+
+func GetFileToWrite(filename string) *os.File {
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0664)
+	if err != nil {
+		log.Fatal("Cannot open or create csv file: %s, error: %v", filename, err)
+	}
+	return file
+}
---
+++ utils/id.go
@@ -0,0 +1,25 @@
+package utils
+
+import (
+	"encoding/csv"
+	"os"
+	"strconv"
+)
+
+func GetNextID(file *os.File) int {
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil || len(records) < 1 {
+		return 1
+	}
+
+	var maxID int = 0
+	for i := 1; i < len(records); i++ {
+		id, err := strconv.Atoi(records[i][0])
+		if err == nil && id > maxID {
+			maxID = (id)
+		}
+	}
+
+	return maxID
+}
