c7ec47ea8669044bd30f4d08e9f41374afe06351
Abhishek2010DevSingh <abhishekkumar2010dev@gmail.com>
2025-03-04 09:27:11 +0530

    rootCmd done

--- cmd/root.go
+++ cmd/root.go
@@ -3,6 +3,7 @@ package cmd
 import "github.com/spf13/cobra"
 
 func Init() {
+	// rootCmd represents the base command when called without any subcommands
 	var rootCmd = &cobra.Command{}
 	rootCmd.AddCommand(AddCommand())
 	rootCmd.AddCommand(ListCommand())
