fd9168a43a5689f872d1b89e52cc55b24283a03e
Abhishek2010dev <abhishekkumar2010dev@gmail.com>
2025-06-21 15:28:20 +0530

    using charm

--- cmd/root.go
+++ cmd/root.go
@@ -1,13 +1,47 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"context"
+
+	"github.com/charmbracelet/fang"
+	"github.com/spf13/cobra"
+)
 
 func Init() {
 	// rootCmd represents the base command when called without any subcommands
-	var rootCmd = &cobra.Command{}
+	rootCmd := &cobra.Command{
+		Use:   "Expense Tracker",
+		Short: "📊 A simple CLI to track your expenses",
+		Long:  `Track, list, and manage your daily expenses from the command line.`,
+		Example: `
+# ➕ Add an expense
+go run . add --description "Lunch" --amount 20
+
+# 📋 List all expenses
+go run . list
+
+# 📈 View total expense summary
+go run . summary
+
+# ❌ Delete an expense
+go run . delete --id 2
+
+# 📅 Monthly summary for August
+go run . summary --month 8
+
+# 🧪 Mix and match arguments
+DESCRIPTION="Dinner" AMOUNT=10 go run . add --description "$DESCRIPTION" --amount $AMOUNT
+
+# 🧵 Multi-line environment setup with execution
+ENV_DB_PATH="./db.json" \
+  USER_ID=42 \
+  go run . summary --month 6
+`,
+	}
 	rootCmd.AddCommand(AddCommand())
 	rootCmd.AddCommand(ListCommand())
 	rootCmd.AddCommand(SummaryCommand())
 	rootCmd.AddCommand(DeleteCommand())
-	rootCmd.Execute()
+
+	fang.Execute(context.TODO(), rootCmd)
 }
